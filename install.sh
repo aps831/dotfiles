1baf9322561a840aea0f3d310763ecc4e511355b
Andrew Spratley <aps831@yahoo.co.uk>
2022-02-09 10:39:12 +0000

    feat: added devcontainers and codespaces

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -e
+
+if [ ! "$(command -v chezmoi)" ]; then
+  bin_dir="$HOME/.local/bin"
+  chezmoi="$bin_dir/chezmoi"
+  if [ "$(command -v curl)" ]; then
+    sh -c "$(curl -fsLS https://chezmoi.io/get)" -- -b "$bin_dir"
+  elif [ "$(command -v wget)" ]; then
+    sh -c "$(wget -qO- https://chezmoi.io/get)" -- -b "$bin_dir"
+  else
+    echo "To install chezmoi, you must have curl or wget installed." >&2
+    exit 1
+  fi
+else
+  chezmoi=chezmoi
+fi
+
+script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
+exec "$chezmoi" init --apply "--source=$script_dir"
