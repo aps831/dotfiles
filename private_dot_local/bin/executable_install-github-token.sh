a1ca3d49a084ce8762d69dd092cd033c2ceaf673
Andrew Spratley <aps831@yahoo.co.uk>
2022-02-16 22:16:35 +0000

    feat: add error handling in install-github-token

--- private_dot_local/bin/executable_install-github-token.sh
+++ private_dot_local/bin/executable_install-github-token.sh
@@ -2,11 +2,30 @@
 
 export AWS_PROFILE="andrew-p-spratley"
 
-PATH_USER="/devcontainer/$PROJECT_NAME/github-user"
-PATH_TOKEN="/devcontainer/$PROJECT_NAME/github-token"
-
 aws sso login
-echo "GITHUB_USER=$(aws ssm get-parameter --name "$PATH_USER" --with-decryption | jq '.Parameter.Value' )" > $HOME/.devcontainers.env
-echo "GITHUB_TOKEN=$(aws ssm get-parameter --name "$PATH_TOKEN" --with-decryption | jq '.Parameter.Value' )">> $HOME/.devcontainers.env
+echo "GITHUB_USER=$(aws ssm get-parameter --name "/devcontainer/$PROJECT_NAME/github-user" --with-decryption 2>/dev/null | jq '.Parameter.Value' )" > $HOME/.devcontainers.env
+echo "GITHUB_TOKEN=$(aws ssm get-parameter --name "/devcontainer/$PROJECT_NAME/github-token" --with-decryption 2>/dev/null | jq '.Parameter.Value' )">> $HOME/.devcontainers.env
+
+export $(grep -v '^#' "$HOME/.devcontainers.env" | xargs)
+
+if [ -z "$PROJECT_NAME" ]; then
+    /usr/bin/printf "\u2718 PROJECT_NAME environment variable not read\n"    
+else
+    /usr/bin/printf "\u2714 PROJECT_NAME environment variable read\n"
+fi
+
+if [ -z "$GITHUB_USER" ]; then
+    /usr/bin/printf "\u2718 GITHUB_USER environment variable not set\n"    
+else
+    /usr/bin/printf "\u2714 GITHUB_USER environment variable set\n"
+fi
+
+if [ -z "$GITHUB_TOKEN" ]; then
+    /usr/bin/printf "\u2718 GITHUB_TOKEN environment variable not set\n"
+else
+    /usr/bin/printf "\u2714 GITHUB_TOKEN environment variable set\n"
+fi
+
+
 
 
