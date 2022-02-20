528749812cb7bd7558a17f8f39907508c693e75b
Andrew Spratley <aps831@yahoo.co.uk>
2022-02-20 09:56:10 +0000

    feat: add install aws cli script

---
+++ private_dot_local/bin/executable_install-aws-cli.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+TMPDIR=$(mktemp -d)
+cd "$TMPDIR"
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+./aws/install
+rm -rf "$TMPDIR"
+
+
+
