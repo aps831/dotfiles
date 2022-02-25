5703019ed933579ce785e74860ddd97f17df3bf8
Andrew Spratley <aps831@yahoo.co.uk>
2022-02-25 21:36:19 +0000

    feat(bin): add implementation of audit and depcheck scripts

--- private_dot_local/bin/executable_audit.sh
+++ private_dot_local/bin/executable_audit.sh
@@ -1,3 +1,19 @@
 #! /bin/bash
 
-echo "Not implemented yet"
+if [[ -f "pom.xml" ]]; then
+
+    mvn org.owasp:dependency-check-maven:check -DfailBuildOnAnyVulnerability=true
+
+elif [[ -f "package.json" ]]; then
+
+    npm audit
+
+else
+
+    echo "No package file found"
+
+fi
+
+
+
+
--- private_dot_local/bin/executable_depcheck.sh
+++ private_dot_local/bin/executable_depcheck.sh
@@ -1,3 +1,16 @@
-#!/bin/bash
+#! /bin/bash
 
-echo "Not implemented yet"
+if [[ -f "pom.xml" ]]; then
+
+    mvn versions:display-plugin-updates
+    mvn versions:display-dependency-updates
+
+elif [[ -f "package.json" ]]; then
+
+    npm outdated
+
+else
+
+    echo "No package file found"
+
+fi
