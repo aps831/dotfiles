38c1642b8aa83711fabc0c677b787e121e523207
Andrew Spratley <aps831@yahoo.co.uk>
2022-04-03 11:09:26 +0100

    feat: use profile.d for user profile

--- private_dot_local/bin/executable_install-github-token.sh
+++ private_dot_local/bin/executable_install-github-token.sh
@@ -3,19 +3,21 @@
 export AWS_PROFILE="andrew-p-spratley"
 
 aws sso login
-echo "GITHUB_USER=$(aws ssm get-parameter --name "/devcontainer/$PROJECT_NAME/github-user" --with-decryption 2>/dev/null | jq '.Parameter.Value' )" > $HOME/.devcontainers.env
-echo "GITHUB_TOKEN=$(aws ssm get-parameter --name "/devcontainer/$PROJECT_NAME/github-token" --with-decryption 2>/dev/null | jq '.Parameter.Value' )">> $HOME/.devcontainers.env
+mkdir "$HOME/.profile.d/"
+echo "#! /bin/bash" > "$HOME/.profile.d/github.sh"
+echo "export GITHUB_USER=$(aws ssm get-parameter --name "/devcontainer/$PROJECT_NAME/github-user" --with-decryption 2>/dev/null | jq '.Parameter.Value' )" >> $HOME/.profile.d/github.sh
+echo "export GITHUB_TOKEN=$(aws ssm get-parameter --name "/devcontainer/$PROJECT_NAME/github-token" --with-decryption 2>/dev/null | jq '.Parameter.Value' )">> $HOME/.profile.d/github.sh
 
-export $(grep -v '^#' "$HOME/.devcontainers.env" | xargs)
+source "$HOME/.profile.d/github.sh"
 
 if [ -z "$PROJECT_NAME" ]; then
-    /usr/bin/printf "\u2718 PROJECT_NAME environment variable not read\n"    
+    /usr/bin/printf "\u2718 PROJECT_NAME environment variable not read\n"
 else
     /usr/bin/printf "\u2714 PROJECT_NAME environment variable read\n"
 fi
 
 if [ -z "$GITHUB_USER" ]; then
-    /usr/bin/printf "\u2718 GITHUB_USER environment variable not set\n"    
+    /usr/bin/printf "\u2718 GITHUB_USER environment variable not set\n"
 else
     /usr/bin/printf "\u2714 GITHUB_USER environment variable set\n"
 fi
