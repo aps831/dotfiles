e55bcf754bb1941ba8d7482a41c4d4b8742f50a8
Andrew Spratley <aps831@yahoo.co.uk>
2023-07-23 09:31:50 +0100

    fix: ensure dir exists for age private key

--- run_before_install-age-private-key.sh
+++ run_before_install-age-private-key.sh
@@ -1,10 +1,12 @@
 #! /bin/bash
 
-AGE_PRIVATE_KEY_FILE="${HOME}/.age/chezmoi.age.txt"
+AGE_PRIVATE_KEY_DIR="${HOME}/.age"
+AGE_PRIVATE_KEY_FILE="${AGE_PRIVATE_KEY_DIR}/chezmoi.age.txt"
 
 if [ -z "$AGE_PRIVATE_KEY" ]; then
   echo "AGE_PRIVATE_KEY is not set. Using private key from ${AGE_PRIVATE_KEY_FILE}"
 else
   echo "AGE_PRIVATE_KEY is set. Writing private key to ${AGE_PRIVATE_KEY_FILE}"
+  mkdir -p ${AGE_PRIVATE_KEY_DIR}
   echo ${AGE_PRIVATE_KEY} > ${AGE_PRIVATE_KEY_FILE}
 fi
