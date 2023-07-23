ecf6bca62d5738488ac2238160afc62188174813
Andrew Spratley <aps831@yahoo.co.uk>
2023-07-23 09:15:31 +0100

    fix: remove test from age private key file name

--- run_before_install-age-private-key.sh
+++ run_before_install-age-private-key.sh
@@ -6,5 +6,5 @@ if [ -z "$AGE_PRIVATE_KEY" ]; then
   echo "AGE_PRIVATE_KEY is not set. Using private key from ${AGE_PRIVATE_KEY_FILE}"
 else
   echo "AGE_PRIVATE_KEY is set. Writing private key to ${AGE_PRIVATE_KEY_FILE}"
-  echo ${AGE_PRIVATE_KEY} > ${AGE_PRIVATE_KEY_FILE}.test
+  echo ${AGE_PRIVATE_KEY} > ${AGE_PRIVATE_KEY_FILE}
 fi
